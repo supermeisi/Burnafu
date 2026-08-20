88d54f80924bb431755725d6ba4bb45cc89317e0
Mustafa Schmidt <mustafa.schmidt@uni-siegen.de>
2026-08-20 19:22:48 +0200

    Adding header

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
+<?php
+session_start();
+
+$visitors = 0;
+?>
+
 <html>
     <head>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <div class="header">
+            <?php if(!isset($_SESSION['username'])): ?>
+                <a href="/index.php?page=register" rel="nofollow">Register</a> |
+            <?php endif; ?>
+            <?php if(isset($_SESSION['username'])): ?>
+                <a href="/index.php?page=login&logout" rel="nofollow">Logout [<?= $_SESSION['username']; ?>]</a>
+            <?php else: ?>
+                <a href="/index.php?page=login" rel="nofollow">Login</a>
+            <?php endif; ?>
+            | Visitors: <?= $visitors; ?>
+        </div>
     </body>
 </html>
