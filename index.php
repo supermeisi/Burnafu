b85d20526d213934a6c3c89ed27fd4ddb3f6fdda
Mustafa Schmidt <mustafa.schmidt@uni-siegen.de>
2026-08-21 08:10:44 +0200

    Updating code

--- index.php
+++ index.php
@@ -5,23 +5,34 @@ $visitors = 0;
 ?>
 
 <html>
-    <head>
-        <link rel="stylesheet" href="/style.css">
-    </head>
-    <body>
-        <div class="header">
-            <?php if(!isset($_SESSION['username'])): ?>
-                <a href="/index.php?page=register" rel="nofollow">Register</a> |
-            <?php endif; ?>
-            <?php if(isset($_SESSION['username'])): ?>
-                <a href="/index.php?page=login&logout" rel="nofollow">Logout [<?= $_SESSION['username']; ?>]</a>
-            <?php else: ?>
-                <a href="/index.php?page=login" rel="nofollow">Login</a>
-            <?php endif; ?>
-            | Visitors: <?= $visitors; ?>
-        </div>
-        <div class="banner">
-            <h1>Welcome to the website!</h1>
-        </div>
-    </body>
+
+<head>
+    <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+    <div class="header">
+        <?php if (!isset($_SESSION['username'])): ?>
+            <a href="/index.php?page=register" rel="nofollow">Register</a> |
+        <?php endif; ?>
+        <?php if (isset($_SESSION['username'])): ?>
+            <a href="/index.php?page=login&logout" rel="nofollow">Logout [<?= $_SESSION['username']; ?>]</a>
+        <?php else: ?>
+            <a href="/index.php?page=login" rel="nofollow">Login</a>
+        <?php endif; ?>
+        | Visitors: <?= $visitors; ?>
+    </div>
+    <div class="banner">
+        <h1>Welcome to the website!</h1>
+    </div>
+    <div class="navigation">
+        <h3>Navigation</h3>
+        <ul>
+            <li><a href="/index.php?page=home" rel="nofollow">Home</a></li>
+            <li><a href="/index.php?page=about" rel="nofollow">About</a></li>
+            <li><a href="/index.php?page=contact" rel="nofollow">Contact</a></li>
+        </ul>
+    </div>
+</body>
+
 </html>
