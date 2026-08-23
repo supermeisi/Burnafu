5192e4c02f9e84f6db59fd45540bc44cdac31918
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2026-08-23 23:06:41 +0200

    Optimizing layout

--- email.php
+++ email.php
@@ -28,9 +28,9 @@ function send_email($to, $subject, $body) {
         $mail->Body    = $body;
     
         $mail->send();
-        echo 'Message has been sent successfully';
+        //echo 'Message has been sent successfully.';
     } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}.";
     }
 }
 ?>
